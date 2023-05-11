c53a4aaf5b66d0ec4630abe0c549e59d4b0f5495
Eva Tunnicliffe <theappleguy@MacBook-Air-3.local>
2023-05-12 09:50:22 +1200

    Front end site completed

--- server.py
+++ server.py
@@ -1,22 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return"<h1>Hello World</h1>"
+    return render_template("index.html")
+
 
 @app.route('/classes')
 def classes():
-    return"<h1>Classes Page</h1>"
+    return render_template("classes.html")
+
 
 @app.route('/teachers')
 def teachers():
-    return"<h1>Teachers Page</h1>"
+    return render_template("teachers.html")
 
-@app.route('/enrol')
-def enrol(id):
-    return"<h1>Enrol Page</h1> "
+
+@app.route('/enrol', methods=["GET", "POST"])
+def enrol():
+    if request.method == "POST":
+        f = request.form
+        print(f)
+        return render_template("confirm.html", form_data=f)
+    elif request.method == "GET":
+        temp_form_data = {
+            "fullname": "Eva Tunnicliffe",
+            "email": "eva.wgtn@gmail.com",
+            "age": "17",
+            "aboutme": "Hello",
+            "problems": "No",
+            "consent": "Yes",
+            "how": "Through friends"
+        }
+        return render_template("enrol.html", **temp_form_data)
 
 
 if __name__ == "__main__":
