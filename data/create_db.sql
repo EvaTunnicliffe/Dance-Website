082b66bc4627fda59539372ed093bb97a81b7406
Eva Tunnicliffe <theappleguy@MacBook-Air-4.local>
2023-06-20 13:57:44 +1200

    update after 1st database

---
+++ data/create_db.sql
@@ -0,0 +1,54 @@
+/* database creation */
+
+drop table if exists news;
+drop table if exists member;
+
+/* create tables */
+
+create table member(
+    member_id integer primary key autoincrement not null,
+    name text not null,
+    email text not null unique,
+    password text not null,
+    authorisation integer not null
+);
+
+create table news(
+    news_id integer primary key autoincrement not null,
+    title text not null unique,
+    subtitle text not null unique,
+    content text not null unique,
+    newsdate date not null ,
+    member_id integer not null,
+    foreign key(member_id) references member(member_id)
+);
+
+insert into member( name, email, password, authorisation)
+values('Mike', 'm@g.com', 'temp', 0);
+insert into member( name, email, password, authorisation)
+values('Vanessa', 'vanny@yahoo.com', 'temp', 0);
+insert into member( name, email, password, authorisation)
+values('Olivia', 'olly66@marsden.com', 'temp', 1);
+insert into member( name, email, password, authorisation)
+values('Suzie', 'zuzy@qmc.com', 'temp', 1);
+
+
+
+insert into news(title, subtitle, content, newsdate, member_id)
+values('LA Dance Show',
+       'On Sunday the 4th of June',
+       'Come along to enjoy a night at the movies, with songs from all your favourite films!' || char(10) ||
+       'There''s something for everyone.',
+       '2023-05-04 7:00:00',
+       (select member_id from member where name='Mike' )
+       );
+
+insert into news(title, subtitle, content, newsdate, member_id)
+values ('Class Cancelled',
+        'This Wednesday 31st May',
+        'Advanced One jazz class at 7:15pm until 8:30pm cancelled this week due to illness. ' || char(10) ||
+        'This will be made up for at a later date.',
+        '2023-05-29 14:00:00',
+        (select member_id from member where name = 'Vanessa' )
+        );
+
--- server.py
+++ server.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from db_functions import run_search_query_tuples, run_commit_query
+from datetime import datetime
 
 app = Flask(__name__)
+db_path = 'data/dance_db.sqlite'
+
+
+@app.template_filter()
+def newsdate(sqlite_dt):
+    # create a data object
+    x = datetime.strptime(sqlite_dt, '%Y-%m-%d %H:%M:%S')
+    return x.strftime("%a %d %b %y %I:%M %p")
 
 
 @app.route('/')
@@ -35,6 +45,102 @@ def enrol():
             "how": "Through friends"
         }
         return render_template("enrol.html", **temp_form_data)
+
+
+@app.route('/notices')
+def notices():
+    # query for the page
+    sql = """select news.news_id, news.title, news.subtitle, news.content, news.newsdate, member.name
+    from news
+    join member on news.member_id = member.member_id
+    order by news.newsdate desc;
+    """
+
+    result = run_search_query_tuples(sql, (), db_path, True)
+    print(result)
+    return render_template("notices.html", news=result)
+
+
+@app.route('/news_cud', methods=['GET', 'POST'])
+def news_cud():
+    # collect data from the web address
+    data = request.args
+    print(data)
+    required_keys = ['id', 'task']
+    for k in required_keys:
+        if k not in data.keys():
+            message = " Could not get the right keys for news CUD"
+            return render_template("error.html", message=message)
+    # have an id and a task key
+    if request.method == 'GET':
+        if data['task'] == 'delete':
+            sql = "delete from news where news_id =?"
+            values_tuple = (data['id'],)
+            result= run_commit_query(sql, values_tuple, db_path)
+            return redirect(url_for('notices'))
+
+        elif data['task'] == 'update':
+            sql = """ select title, subtitle, content from news where news_id=?"""
+            values_tuple = (data['id'],)
+            result = run_search_query_tuples(sql, values_tuple, db_path, True)
+            result = result[0]
+            return render_template("news_cud.html",
+                                   **result,
+                                   id=data['id'],
+                                   task=data['task'])
+
+
+        elif data['task'] == 'add':
+            # dummy data for testing
+            temp = {'title' : 'Test Title' , 'subtitle' : 'Test Subtitle' , 'content' : 'Test Content'}
+            return render_template("news_cud.html",
+                                   id=0,
+                                   task=data['task'],
+                                   **temp)
+        else:
+            message = "Unrecognised task coming from news page"
+            return render_template("error.html", message=message)
+    elif request.method == 'POST':
+        # collected form information
+        f = request.form
+        print(f)
+
+        if data['task'] == 'add':
+            # add the new news entry to the database
+            # member is fixed for now
+            sql = """insert into news(title, subtitle, content, newsdate, member_id)
+                        values(?,?,?,datetime('now', 'localtime'),2)"""
+            values_tuple = (f['title'], f['subtitle'], f['content'])
+            result = run_commit_query(sql, values_tuple, db_path)
+            return redirect(url_for('notices'))
+
+        elif data['task'] == 'update':
+            sql = """update news set title=?, subtitle=?, content=?, newsdate=datetime('now', 'localtime') where news_id=?"""
+            values_tuple = (f['title'], f['subtitle'], f['content'], data['id'])
+            result = run_commit_query(sql, values_tuple, db_path)
+            # collect the data from the form and update the database at the sent id
+            return redirect(url_for('notices'))
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("/login.html", email='vanny@yahoo.com', password='temp')
+    elif request.method == "POST":
+        f=request.form
+        print(f)
+        sql = """select name, password, authorisation from member where email = ?"""
+        values_tuple=(f['email'],)
+        result = run_search_query_tuples(sql, values_tuple, db_path, True)
+        if result:
+            result = result[0]
+            print(result)
+            print(result['name'])
+            print(result['password'])
+            print(result['authorisation'])
+            return "<h1> Result is recognised</h1>"
+        else:
+            error = "Your credentials are not recognised"
+            return render_template("/login.html", email='vanny@yahoo.com', password='temp', error=error)
 
 
 if __name__ == "__main__":
